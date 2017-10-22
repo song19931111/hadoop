a18dec243ba77a3aa5ff9bcbeaa4beec63ac92a2
A U Thor <41456@qq.com>
2017-10-22 21:58:41 +0800

    "Test2"

--- src/test/java/my/hadoopstudy/AppTest.java
+++ src/test/java/my/hadoopstudy/AppTest.java
@@ -1,11 +1,15 @@
 package my.hadoopstudy;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URI;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.LocatedFileStatus;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.RemoteIterator;
 import org.apache.hadoop.fs.permission.FsPermission;
 import org.junit.Before;
 import org.junit.Test;
@@ -28,7 +32,7 @@ public class AppTest
 	}
 	@Test
 	public void testUpload() throws IllegalArgumentException, IOException{
-		fs.copyFromLocalFile(new Path("C://adcfg.json"),new Path ("/test/abcfg.json"));
+		fs.delete(new Path("mkdir"),true); //ÊÇ·ñµÝ¹éÉ¾³ý
 		fs.close();
 	}
 	
@@ -43,5 +47,32 @@ public class AppTest
     public void TestMkdir() throws IllegalArgumentException, IOException{
     	boolean mkdirs = fs.mkdirs(new Path("/testDir"),FsPermission.getDefault());
     	System.out.println(mkdirs);
+    }
+    @Test
+    public void TestDelete() throws IllegalArgumentException, IOException{
+    	boolean mkdirs = fs.mkdirs(new Path("/testDir"),FsPermission.getDefault());
+    	System.out.println(mkdirs);
+    }
+    @Test
+    public void TestList() throws FileNotFoundException, IllegalArgumentException, IOException{
+    	//true ÊÇ·ñµÝ¹é
+    	RemoteIterator<LocatedFileStatus> listFiles = fs.listFiles(new Path("/"),false); 
+    	while(listFiles.hasNext()){
+    		LocatedFileStatus fileStatus = listFiles.next();
+//    		System.out.println(fileStatus.getBlockSize());
+//    		System.out.println(fileStatus.getOwner());
+    		System.out.println("NAME1:"+fileStatus.getPath());
+//    		System.out.println("NAME2:"+fileStatus.getClass().getName());
+//    		System.out.println(fileStatus.getReplication());
+    	}
+    
+    }
+    @Test
+    public void TestListStatus() throws FileNotFoundException, IllegalArgumentException, IOException{
+    	FileStatus[] listStatus = fs.listStatus(new Path("/"));
+    	for(FileStatus status:listStatus){
+    		System.out.println(status.getPath().getName());
+    	}
+    
     }
 }
